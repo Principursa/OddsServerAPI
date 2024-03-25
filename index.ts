18e1d267e216d8f204ca5a7e653418f5097edb23
Pattisier <Principursa@protonmail.com>
2024-03-25 14:58:42 -0400

    finished odds route

--- index.ts
+++ index.ts
@@ -15,8 +15,8 @@ interface gameObject {
   id: string;
   home_team: string;
   away_team: string;
-  home_odds: number;
-  away_odds: number;
+  home_points: number;
+  away_points: number;
   commence_time: string; //in unix so smart contract conversion is easier
 }
 
@@ -32,20 +32,28 @@ app.get("/list", (req, res, next) => {
     .catch((err) => next(err));
 });
 app.get("/scoresbyid/:id", (req, res, next) => {
-  const specscoreslink = `https://api.the-odds-api.com/v4/sports/basketball_nba/scores/?daysFrom=1&apiKey=${apiKey}`
+  const specscoreslink = `https://api.the-odds-api.com/v4/sports/basketball_nba/scores/?daysFrom=1&apiKey=${apiKey}&eventIds=${req.params.id}`
   console.log("scoresbyid" + req.params.id);
   res.send("scoresbyid" + req.params.id);
 });
 
 app.get("/oddsbyid/:id", (req, res, next) => {
-  var date = new Date().toISOString();
-  var newdate = date.slice(0, date.length - 5) + "Z"
-  const specOddsLink = `https://api.the-odds-api.com/v4/sports/basketball_nba/odds?apiKey=${apiKey}&regions=us&markets=h2h&dateFormat=unix&oddsFormat=american&eventIds=${req.params.id}&bookmakers=draftkings&commenceTimeFrom=${newdate}`;
+  const date = returnFormattedDate()
+  const specOddsLink = `https://api.the-odds-api.com/v4/sports/basketball_nba/odds?apiKey=${apiKey}&regions=us&markets=spreads&dateFormat=unix&oddsFormat=american&eventIds=${req.params.id}&bookmakers=draftkings&commenceTimeFrom=${date}`;
   axios
     .get(specOddsLink)
     .then((response) => {
-      let json = stringify(response.data);
-      res.send(json);
+      let id = response.data[0]["id"]
+      let commence_time = response.data[0]["commence_time"]
+      let home_team = response.data[0]["home_team"]
+      let away_team = response.data[0]["away_team"]
+      let home_points = response.data[0]["bookmakers"][0]["markets"][0]["outcomes"][0]["point"]
+      let away_points = response.data[0]["bookmakers"][0]["markets"][0]["outcomes"][1]["point"]
+      let gameinit : gameObject = { id,home_team,away_team,home_points,away_points,commence_time }
+      let json = stringify(response.data[0]["bookmakers"][0]["markets"][0]["outcomes"][0]["point"], null, "\t");
+      console.log(`1:${home_team} \t 2: ${away_team}`)
+      console.log(gameinit)
+      res.send(gameinit);
     })
     .catch((err) => next(err));
 });
@@ -54,3 +62,9 @@ app.listen(port, () => {
   console.log(`NBAAPI app listening on port ${port}`);
 });
 
+function returnFormattedDate(){
+  var date = new Date().toISOString();
+  var newdate = date.slice(0, date.length - 5) + "Z"
+  return newdate
+
+}

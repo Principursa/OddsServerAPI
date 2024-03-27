648c5bd9e84e0d81e1f02030e2ac9a031d7645d6
Pattisier <Principursa@protonmail.com>
2024-03-26 23:08:31 -0400

    api stuff

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import axios, { AxiosResponse } from "axios";
-import express, { Express, Request, response, Response } from "express";
+import express, {
+  Express,
+  request,
+  Request,
+  response,
+  Response,
+} from "express";
 import dotenv from "dotenv";
 import { stringify } from "flatted";
 //TODO: Three different Routes, one for getting list of all games, one for odds for spec game:
 //NOTE: for the contract it should only be necessary to get specific games, frontend can handle listing
 // all available  games
+
 
 dotenv.config();
 const apiKey = process.env.API_KEY;
@@ -24,10 +31,19 @@ interface gameObjectSC {
   away_points: number;
   commence_time: number; //in unix so smart contract conversion is easier
 }
+interface gameResult {
+  id: string;
+  home_team: string;
+  away_team: string;
+  home_score: number;
+  away_score: number;
+  commence_time: number;
+  completed: boolean;
+}
 app.get("/", async (req, res) => {
   res.send("Server for scry hackathon");
 });
-app.get("/list", async (req, res, next) => {
+app.get("game/list", async (req, res, next) => {
   console.log("hello world");
   const date = returnFormattedDate();
   const link = `https://api.the-odds-api.com/v4/sports/basketball_nba/odds?apiKey=${apiKey}&regions=us&markets=spreads&dateFormat=unix&oddsFormat=american&bookmakers=draftkings&commenceTimeFrom=${date}`;
@@ -39,21 +55,33 @@ app.get("/list", async (req, res, next) => {
     json.forEach((info) => {
       idArr.push(info["id"]);
     });
-    console.log(idArr)
+    console.log(idArr);
     for (var id of idArr) {
       var data = await getGameDataFE(id, date);
       gameInfoArr.push(data);
     }
-    console.log(gameInfoArr)
-    res.send(gameInfoArr)
+    console.log(gameInfoArr);
+    res.send(gameInfoArr);
   } catch (err) {
     throw err;
   }
 });
-app.get("/game-result/:id", (req, res, next) => {
-  const specscoreslink = `https://api.the-odds-api.com/v4/sports/basketball_nba/scores/?daysFrom=1&apiKey=${apiKey}&eventIds=${req.params.id}`;
-  console.log("scoresbyid" + req.params.id);
-  res.send("scoresbyid" + req.params.id);
+app.get("/game-result/:id", async (req, res, next) => {
+  const result = await getGameResult(req.params.id,3)
+  res.send(result)
+});
+
+app.get("/game-result/list", async (req, res, next) => {
+  const scoresListLink = ` https://api.the-odds-api.com/v4/sports/basketball_nba/scores/?apiKey=${apiKey}&daysFrom=3&dateFormat=unix`
+  console.log(scoresListLink)
+  try{
+    const response = await axios.get(scoresListLink)
+    const json = response.data[0]
+    res.send(json)
+
+  }catch(err){
+    throw(err)
+  }
 });
 
 app.get("/game/:id", async (req, res) => {
@@ -61,6 +89,14 @@ app.get("/game/:id", async (req, res) => {
   var gmInfo = await getGameDataFE(req.params.id, date);
   res.send(gmInfo);
 });
+
+app.get("/oracle/game/:id", async (req, res) => {
+  res.send("test");
+});
+app.get("/oracle/game-result/:id", async (req,res) => {
+  res.send("test")
+
+})
 
 app.listen(port, () => {
   console.log(`NBAAPI app listening on port ${port}`);
@@ -93,10 +129,31 @@ async function getGameDataFE(id: string, date: string): Promise<gameObjectFE> {
     throw err;
   }
 }
+async function getGameResult(id: string, daysFrom: number) {
+  const scoreLink = `https://api.the-odds-api.com/v4/sports/basketball_nba/scores?apiKey=${apiKey}&daysFrom=${daysFrom}&dateFormat=unix&eventIds=${id}`;
+  try {
+    const response = await axios.get(scoreLink);
+    const json = response.data[0];
+    const id = json["id"]
+    const home_team = json["home_team"]
+    const away_team = json["away_team"]
+    const home_score = json["scores"][0]["score"]
+    const away_score = json["scores"][1]["score"]
+    const commence_time = json["commence_time"]
+    const completed =  json["completed"]
+    const gameResult : gameResult = {id,home_team,away_team, home_score, away_score, commence_time,completed}
+    return gameResult
+
+  } catch (err) {
+    console.log(err)
+    throw(err)
+  }
+}
 
 function returnFormattedDate() {
   var date = new Date().toISOString();
   var newdate = date.slice(0, date.length - 5) + "Z";
+  console.log("date", newdate);
   return newdate;
 }
 
